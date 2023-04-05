88a1092fe640ecd7475be185cc22cefda19795c9
Vlad <curcan.vlad@yahoo.com>
2023-04-05 19:45:13 +0300

    Initial commit

---
+++ GFK/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GFK.Services;
+
+
+namespace GFK.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly TwitterApi _twitterApi;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            _twitterApi = new TwitterApi("uXsW4M7Q4M2zoDddLxRHoAt04", "h104iVd1Y0xnrdaj2B4oYpEpDFrK1iPiNaAqJekbhEOmQxTKIR", "1643370976070057986-LmCYF4ipYyTvhViWY41UwewUvo4QRZ", "5HtXMVhnUFxzQDeIZejHcfcFZXbo7BR8qq0QRwF8XEOpp");
+        }
+
+        public async Task<IActionResult> Index(string query, bool onlyWithImages = false)
+        {
+            if (!string.IsNullOrEmpty(query))
+            {
+                ViewBag.Tweets = await _twitterApi.SearchTweetsAsync(query, onlyWithImages);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ GFK/Services/TwitterApi.cs
@@ -0,0 +1,36 @@
+﻿using Tweetinvi;
+using Tweetinvi.Models.V2;
+using Tweetinvi.Parameters.V2;
+
+namespace GFK.Services
+{
+    public class TwitterApi
+    {
+        private readonly ITwitterClient _twitterClient;
+
+        public TwitterApi(string consumerKey, string consumerSecret, string accessToken, string accessTokenSecret)
+        {
+            _twitterClient = new TwitterClient("uXsW4M7Q4M2zoDddLxRHoAt04", "h104iVd1Y0xnrdaj2B4oYpEpDFrK1iPiNaAqJekbhEOmQxTKIR", "1643370976070057986-LmCYF4ipYyTvhViWY41UwewUvo4QRZ", "5HtXMVhnUFxzQDeIZejHcfcFZXbo7BR8qq0QRwF8XEOpp");
+        }
+
+        public async Task<List<TweetV2>> SearchTweetsAsync(string query, bool onlyWithImages)
+        {
+            var searchParameter = new SearchTweetsV2Parameters(query)
+            {
+                PageSize = 100
+            };
+
+                var searchResponse = await _twitterClient.SearchV2.SearchTweetsAsync(searchParameter);
+                var tweets = searchResponse.Tweets.ToList();
+
+                if (onlyWithImages)
+                {
+                    tweets = tweets.Where(t => t.Attachments != null && t.Attachments.MediaKeys != null && t.Attachments.MediaKeys.Length > 0).ToList();
+                }
+
+                return tweets;
+        }
+    }
+}
+
+
